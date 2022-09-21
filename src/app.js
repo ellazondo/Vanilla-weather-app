ff0dd19d84a3ab78b1ae037a75e226dca2d899aa
ellazondo <gabriellaelizondo@gmail.com>
2022-09-21 10:40:27 -0500

    starting to add js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector(#city);
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innherHTML = response.data.name;
+}
+
+let apiKey = "7e51999498b98449960c3d517772a9e2";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Amsterdam&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
+
 function conversionC(event) {
   event.preventDefault();
   let tempClickC = document.querySelector("#temp-click");
