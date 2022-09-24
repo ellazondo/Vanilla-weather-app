07a8e63c1de92a15691392a967e867e2f9af5e24
ellazondo <gabriellaelizondo@gmail.com>
2022-09-24 15:09:31 -0500

    remove displayFahrenheit func

--- src/app.js
+++ src/app.js
@@ -111,15 +111,6 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-
 function displayCelsiusTemperature(event) {
   event.preventDefault();
   celsiusLink.classList.add("active");
